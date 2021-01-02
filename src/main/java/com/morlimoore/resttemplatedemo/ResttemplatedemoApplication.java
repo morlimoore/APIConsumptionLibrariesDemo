4442954ffbb812723cf9fd25be1b169731b38ab2
morlimoore <cooliyk2@gmail.com>
2021-01-02 01:06:08 +0100

    Initial commit

---
+++ src/main/java/com/morlimoore/resttemplatedemo/ResttemplatedemoApplication.java
@@ -0,0 +1,12 @@
+package com.morlimoore.resttemplatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttemplatedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttemplatedemoApplication.class, args);
+	}
+}
