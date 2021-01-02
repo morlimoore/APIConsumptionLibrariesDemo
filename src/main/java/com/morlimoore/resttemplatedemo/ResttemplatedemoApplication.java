e42debef085cb43aa8bceb16e04ada25753122b6
morlimoore <cooliyk2@gmail.com>
2021-01-02 01:07:10 +0100

    Creates RestTemplate bean

--- src/main/java/com/morlimoore/resttemplatedemo/ResttemplatedemoApplication.java
+++ src/main/java/com/morlimoore/resttemplatedemo/ResttemplatedemoApplication.java
@@ -2,11 +2,18 @@ package com.morlimoore.resttemplatedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ResttemplatedemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResttemplatedemoApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 }
