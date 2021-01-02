d675e947c413a23e978d839747e072b3b53b8aa9
morlimoore <cooliyk2@gmail.com>
2021-01-02 02:15:31 +0100

    Creates User class

---
+++ src/main/java/com/morlimoore/resttemplatedemo/models/User.java
@@ -0,0 +1,11 @@
+package com.morlimoore.resttemplatedemo.models;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+}
