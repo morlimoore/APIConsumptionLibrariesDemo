173a1616144d9da53527ac4abb1e17c9c955c7e2
morlimoore <cooliyk2@gmail.com>
2021-01-06 09:04:46 +0100

    Adds validator

--- src/main/java/com/morlimoore/APIConsumptionLibrariesDemo/models/User.java
+++ src/main/java/com/morlimoore/APIConsumptionLibrariesDemo/models/User.java
@@ -3,14 +3,22 @@ package com.morlimoore.APIConsumptionLibrariesDemo.models;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Getter
 @Setter
 public class User implements Serializable {
 
+    @NotBlank(message = "First name cannot be blank")
     private String firstName;
+
+    @NotBlank(message = "Last name cannot be blank")
     private String lastName;
+
+    @Email(message = "Enter a valid email address")
+    @NotBlank(message = "Email cannot be blank")
     private String email;
 
     @Override
